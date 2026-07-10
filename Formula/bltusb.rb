312aca6a47e2cca546bce3d33c6877d2a50bb8fb
neil0306 <719860478@qq.com>
2026-07-10 12:41:50 +0800

    bltusb 1.3.1

--- Formula/bltusb.rb
+++ Formula/bltusb.rb
@@ -1,8 +1,8 @@
 class Bltusb < Formula
   desc "Read/write BitLocker-encrypted USB drives on macOS (Apple Silicon)"
   homepage "https://github.com/neil0306/bltusb"
-  url "https://github.com/neil0306/bltusb/archive/refs/tags/v1.3.0.tar.gz"
-  sha256 "753265f80bd60bff437f47c2d5aba9e56ce144f1ac852f01e49de7b5d1204097"
+  url "https://github.com/neil0306/bltusb/archive/refs/tags/v1.3.1.tar.gz"
+  sha256 "0639c70177ea8ebd891592334a12c014a85e40f44c00ec752620f82e9b12eb80"
   license "MIT"
 
   depends_on :macos
